8c42309815dd809e8fd65be4317428373616d722
zacharywray <zacharywray@gmail.com>
2024-04-15 14:15:20 -0400

    init commit

---
+++ AStar.h
@@ -0,0 +1,14 @@
+//
+// Created by Zach's PC on 4/13/2024.
+//
+
+#ifndef ASTAR_ASTAR_H
+#define ASTAR_ASTAR_H
+
+
+class AStar {
+
+};
+
+
+#endif //ASTAR_ASTAR_H
---
+++ Main.h
@@ -0,0 +1,14 @@
+//
+// Created by Zach's PC on 4/13/2024.
+//
+
+#ifndef ASTAR_MAIN_H
+#define ASTAR_MAIN_H
+
+
+class Main {
+
+};
+
+
+#endif //ASTAR_MAIN_H
